07818be539a25396b5985553cee88ef057d0d760
joshfollmer <josh.follmer@icloud.com>
2021-01-20 15:00:33 -0800

    Update queue.py

--- queue.py
+++ queue.py
@@ -6,14 +6,19 @@ class Queue:
         self.list = list
 
     def append(self, new_item):
+        #inserts the passed item into the first place
         self.list.insert(0, new_item)
         return self.list
 
     def pop(self):
-        del self.list[-1]
-        return self.list
+        #saves the last item as a variable
+        removed_item = self.list[-1]
+        #removes the last item from the  list
+        del self.list[removed_item]
+        return self.list, removed_item
 
     def contains(self, check_item):
+        #loops through, if the item to be checked is in the list, return true. otherwise return false
         for i in self.list:
             if i == check_item:
                 return True
