e57bca114489015142b0740869a2ec91e173433e
joshfollmer <josh.follmer@icloud.com>
2021-03-03 16:32:17 -0800

    blah

---
+++ sign_out_front.py
@@ -0,0 +1,37 @@
+import circular_linked_list
+from random import randint
+import itertools
+
+class Student:
+    def __init__(self):
+        self.slides = circular_linked_list.CircularLinkedList()
+        #populates the list with slides
+        for i in range(1, 21):
+            self.slides.append_right(f"Slide {i}")
+
+    def simulate(self):
+        temp = self.slides.head
+        for i in range(randint(1, 21)):
+            temp = temp.next
+
+        seen_slides = []
+        seen_slides.append(temp.data)
+        for i in range(4):
+            temp = temp.next
+            seen_slides.append(temp.data)
+        return seen_slides
+
+    def store_slides(self):
+        total_slides = []
+        for i in range(4):
+            total_slides.append(s.simulate())
+        total_slides = list(itertools.chain.from_iterable(total_slides))
+        
+        total_slides = set(total_slides)
+        print(len(total_slides))
+        
+
+
+
+s = Student()
+s.store_slides()
