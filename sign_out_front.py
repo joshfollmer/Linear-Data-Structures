a6b0596f61022891e767b195c1817654afc36efb
joshfollmer <josh.follmer@icloud.com>
2021-03-10 13:06:06 -0800

    Update sign_out_front.py

--- sign_out_front.py
+++ sign_out_front.py
@@ -2,9 +2,9 @@ import circular_linked_list
 from random import randint
 from random import choice
 import itertools
-from datetime import datetime
+from datetime import datetime, time
 from datetime import timedelta
-
+from math import ceil
 
 
 class Sign:
@@ -18,20 +18,17 @@ class Sign:
 class Student:
     def __init__(self):
         arrive_times = [datetime(2021, 1, 1,8), datetime(2021, 1, 1,9,40), datetime(2021, 1, 1,11,10), datetime(2021, 1, 1,12,50), datetime(2021, 1, 1,14,30), datetime(2021, 1, 1,16,50), datetime(2021, 1, 1,17, 50), datetime(2021, 1, 1,19)]
-        
+        names = ["Lucas", "Will", "Noah", "Alex", "Josh", "Doug", "Mike", "John", "David", "Aaron", "Joye", "Abby", "Sofia", "Fiona", "Zoey", "Lily", "Hannah", "Bella", "Anna", "Elena"]
         self.mw = choice(arrive_times)
         self.tt = choice(arrive_times)
         self.punc = randint(1, 10)
-
-        self.name = choice("Lucas", "Will", "Noah", "Alex", "Josh", "Doug", "Mike", "John", "David", "Aaron", "Joye", "Abby", "Sofia", "Fiona", "Zoey", "Lily", "Hannah", "Bella", "Anna", "Elena", "Alice")
-
-
-        
+        self.seen_slides = []
+        self.name = choice(names)
  
     def simulate(self, day):
-        temp = sign.slides.head
+        self.temp = sign.slides.head
         for i in range(randint(1, 21)):
-            temp = temp.next
+            self.temp = self.temp.next
         
         
 
@@ -42,43 +39,51 @@ class Student:
 
         tardy = randint(1,2)
         if tardy == 1:
-            arrive.minute -= self.punc
+            arrive -= timedelta(minutes=self.punc)
             self.speed = randint(20, 30)
         if tardy == 2:
-            arrive.minute += self.punc
-            self.speed = randint(30, 40)
-
-            
+            arrive += timedelta(minutes=self.punc)
+            self.speed = randint(30, 40)     
 
         t = datetime(2021, 1, 1, 7,50)
-        while t.hour < 20:
-            temp = temp.next
-            if t.time == arrive
-            t.second += 20
 
-        # seen_slides = []
-        # seen_slides.append(temp.data)
-        # for i in range(4):
-        #     temp = temp.next
-        #     seen_slides.append(temp.data)
-        # return seen_slides
+        for i in range(2046):
+            self.temp = self.temp.next
+            if t == arrive:
+                self.see_slides()
+            t += timedelta(seconds=20)
+
+ 
 
     def see_slides(self):
-
-
-    # def store_slides(self):
-    #     total_slides = []
-    #     for i in range(4):
-    #         total_slides.append(s.simulate())
-    #     total_slides = list(itertools.chain.from_iterable(total_slides))
+        #we need to find out how long they can see the sign for
+        #first we find the feet per second, found by multiplying their current speed by 1.47
+        fps = self.speed * 1.47
+        #we divide the number of feet where they can see the sign and divide it by the feet per second, giving us the number of seconds they can see the sign for
+        #i found 2205 by taking the mean amount of time of 60 seconds, assuming thats at the speed limit of 25 mph, finding the ft/s of 25 nad multiplying that by 60
+        time_seeing = 2205 / fps
+        #number of slides seen is the number of seconds divided by 20, rounded up. note that a decimal number refers to the amount of time seeing the slide, not the slide itself
+        num_of_slides = ceil(time_seeing / 20)
         
-    #     total_slides = set(total_slides)
-    #     dec = len(total_slides) / 20
-    #     percent = '{:.0%}'.format(dec)
-    #     print(percent)
+        for i in range(num_of_slides):
+            self.seen_slides.append(self.temp)
+            self.temp = self.temp.next
         
-
+        
+def start_program():
+    #try:
+    start = input("Start simulation? Y/N\n")
+    if start == 'Y' or start == 'y' or start == 'yes' or start == 'Yes':
+        s = Student()
+        for i in range(1, 5):
+            s.simulate(i)
+        s.seen_slides = set(s.seen_slides)
+        print(f"{s.name} saw {len(s.seen_slides)} slides")
+    elif start == 'N' or start == 'n' or start == 'no' or start == 'No':
+        return          
+    # except:
+    #     print("Please input valid command")
+    #     start_program()
 
 sign = Sign()
-s = Student()
-s.store_slides()
+start_program()
--- test.py
+++ test.py
@@ -1,15 +1,3 @@
-
-from datetime import datetime
-from datetime import timedelta
-
-#t = time(7,50)
-#print(t)
-# for i in range(100):
-#     t += datetime.timedelta(seconds = 3)
-#     print(t.time())
- 
-#t = time(8, 10, 20)
-t = datetime(2021, 1, 1, 23, 59, 59)
-print(t.time())
-x = t + timedelta(seconds = 3)
-print(x.time())
+l = [1, 2, 3, 1, 2,3]
+f = set(l)
+print(f)
