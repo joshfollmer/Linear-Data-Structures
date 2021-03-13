a7fd6cbc0f679efd11f8865d75f80a4ebbf67c1c
joshfollmer <josh.follmer@icloud.com>
2021-03-12 17:13:35 -0800

    blah

--- sign_out_front.py
+++ sign_out_front.py
@@ -5,7 +5,6 @@ from datetime import datetime
 from datetime import timedelta
 from math import ceil
 
-
 class Sign:
     def __init__(self):
         self.slides = circular_linked_list.CircularLinkedList()
@@ -13,9 +12,8 @@ class Sign:
         for i in range(1, 21):
             self.slides.append_right(f"Slide {i}")
 
-
 class Student:
-    def __init__(self):
+    def __init__(self, min_speed_lower, max_speed_lower, min_speed_higher, max_speed_higher, min_punc, max_punc, ):
         #makes a list of arrival times. it is in the format of year, month, day, hour, minute. the date must be included so the time can be added to or subtracted from (which is think is dumb)
         arrive_times = [datetime(2021, 1, 1,8), datetime(2021, 1, 1,9,40), datetime(2021, 1, 1,11,10), datetime(2021, 1, 1,12,50), datetime(2021, 1, 1,14,30), datetime(2021, 1, 1,16,50), datetime(2021, 1, 1,17, 50), datetime(2021, 1, 1,19)]
         names = ["Lucas", "Will", "Noah", "Alex", "Josh", "Doug", "Mike", "John", "David", "Aaron", "Joye", "Abby", "Sofia", "Fiona", "Zoey", "Lily", "Hannah", "Bella", "Anna", "Elena"]
@@ -23,15 +21,18 @@ class Student:
         self.mw = choice(arrive_times)
         self.tt = choice(arrive_times)
         #this is their punctuality, or the % chance of them being late on any given day
-        self.punc = randint(1, 10)
+        self.punc = randint(min_punc, max_punc)
         self.seen_slides = []
+        self.total_slides = []
         self.name = choice(names)
         self.temp = sign.slides.head
+        self.min_speed_lower = min_speed_lower
+        self.max_speed_lower = max_speed_lower
+        self.min_speed_higher = min_speed_higher
+        self.max_speed_higher = max_speed_higher
  
-    def simulate(self, day):
-        #picks a random slide to start the day at
-        for i in range(randint(1, 21)):
-            self.temp = self.temp.next  
+    def simulate(self, day, cycle_24):
+
         #day 1 is monday, day 2 is tuesday, etc
         if day == 1 or day == 3:
             arrive = self.mw 
@@ -45,22 +46,29 @@ class Student:
             # if they are early, subtract some time amount of minutes between one and ten from their arrival time
             arrive -= timedelta(minutes=randint(1, 10))
             #if theyre early, theyll be more likely to drive slowly, and see more slides
-            self.speed = randint(20, 30)
+            self.speed = randint(self.min_speed_lower, self.max_speed_lower)
         else:
             #opposite is true for if they are late
             arrive += timedelta(minutes=randint(1,10))
-            self.speed = randint(30, 40)  
-        #starts the day at 7:50
-        t = datetime(2021, 1, 1, 7,50)
+            self.speed = randint(self.min_speed_higher, self.max_speed_higher) 
+
+        if cycle_24 == True:
+             t = datetime(2021, 1, 1, 0)
+             loops = 4320
+        else:
+            t = datetime(2021, 1, 1, 7,50)
+            loops = 2046
+            #picks a random slide to start the day at
+            for i in range(randint(1, 21)):
+                self.temp = self.temp.next 
+        
         #this loop will simulate the school day, in 20 second increments. if the time of the day is equal to their arrival time, it will run the function to see the slides
         #keep in mind that if the arrival time is altered by seconds, this whole thing falls apart because it has to be a multiple of 20 seconds
-        for i in range(2046):
+        for i in range(loops):
             self.temp = self.temp.next
             if t == arrive:
                 self.see_slides()
             t += timedelta(seconds=20)
-
- 
 
     def see_slides(self):
         #we need to find out how long they can see the sign for
@@ -73,18 +81,10 @@ class Student:
         num_of_slides = ceil(time_seeing / 20)
         
         for i in range(num_of_slides):
+            #the reason there is two is because seen slides will get cleared each week but total slides will not cleared
             self.seen_slides.append(self.temp)
+            self.total_slides.append(self.temp)
             self.temp = self.temp.next
         
-        
-def start_program():
-    s = Student()
-    for i in range(1, 5):
-        s.simulate(i)
-    s.seen_slides = set(s.seen_slides)
-    print(f"{s.name} saw {len(s.seen_slides)} slides")
-           
-    
 
 sign = Sign()
-start_program()
